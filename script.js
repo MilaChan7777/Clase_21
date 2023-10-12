2f645a04b351b8744f77ffa6f850f00013551165
MilaChan7777 <camila.lerma1@u.icesi.edu.co>
2023-10-12 08:18:02 -0500

    K pro

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-import { obtenerData } from "./utils.js";
+import { obtenerData, Persona } from "./utils.js";
 
 const render = async () => {
     const data = await obtenerData();
-    console.log(data);
-}
+    
+    const personas = data.personas;
+    const contenedor = document.querySelector("#contenedor");
 
-const personas = data.personas;
-const contenedor = document.querySelector("#contenedor");
-
-for(const perosna of personas) {
-    const personaObj = new Persona (persona.nombre, persona.edad);
+for(const persona of personas) {
+    const personaObj = new Persona (persona.nombre, persona.edad, persona.pais);
     const p = personaObj.render();
     contenedor.appendChild(p);
+    }
 }
 
 window.onload = render;
--- utils.js
+++ utils.js
@@ -1,16 +1,24 @@
 export class Persona {
     nombre = "";
     edad = 0;
+    pais = "";
 
-    constructor(nombre, edad) {
+    constructor(nombre, edad, pais) {
         this.nombre = nombre;
         this.edad = edad;
+        this.pais = pais;
     }
 
     render() {
-        const texto = "Hola, mi nombre es " + this.nombre + " y tengo " + this.edad + "años";
+        const texto = "Hola, mi nombre es " + this.nombre + " y tengo " + this.edad + " años" + " y soy de " + this.pais ;
         const p = document.createElement("p");
         p.textContent = texto;
         return p;
     }
+}
+
+export const obtenerData = async () => {
+    const response = await fetch("https://raw.githubusercontent.com/MilaChan7777/Clase_21/main/data.json");
+    const data = await response.json();
+    return data;
 }
